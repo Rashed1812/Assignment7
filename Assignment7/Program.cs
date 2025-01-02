38e3bdb3801e91740ba9c91f5a054b6411e93490
Your Name <you@example.com>
2025-01-02 18:30:47 +0200

    Answer Q1

--- Assignment7/Program.cs
+++ Assignment7/Program.cs
@@ -2,9 +2,56 @@
 {
     internal class Program
     {
+        #region 1.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+        //struct Point
+        //{
+        //    public double x { get; set; }
+        //    public double y { get; set; }
+        //    public Point(double _x, double _y)
+        //    {
+        //        x = _x;
+        //        y = _y;
+        //    }
+
+        //    public override string ToString()
+        //    {
+        //        return $"({x},{y})";
+        //    }
+        //}
+        //static Point CreatePoint()
+        //{
+        //    Point point = new Point();
+
+        //    Console.Write("X: ");
+        //    point.x = double.Parse(Console.ReadLine());
+
+        //    Console.Write("Y: ");
+        //    point.y = double.Parse(Console.ReadLine());
+
+        //    return point;
+        //}
+
+        //static double CalcDistance(Point p1, Point p2)
+        //{
+        //    return Math.Sqrt(Math.Pow(p1.x-p2.x, 2) + Math.Pow(p1.y-p2.y, 2));
+        //}
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+
+            //Console.WriteLine("Enter The First Point");
+
+            //Point p1 = CreatePoint();
+
+            //Console.WriteLine("Enter The Second Point");
+
+            //Point p2 = CreatePoint();
+
+            //Console.WriteLine($"Distance = {CalcDistance(p1, p2)}");
+            #endregion
+
         }
     }
 }
